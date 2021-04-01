4ea56992f48df660934e51f408929bb491a884d0
lipa <lipa@opinov8.com>
2021-04-01 23:09:24 +0300

    Initial

---
+++ Rectangles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = new Point[] 
+            {
+                new Point(2, 8),
+                new Point(2, 2),
+                new Point(7, 7),
+                new Point(2, 10),
+                new Point(2, 11),
+                new Point(4, 11),
+                new Point(5, 10),
+                new Point(5, 8),
+                new Point(4, 8),
+                new Point(5, 3),
+                new Point(5, 5),
+                new Point(9, 5), 
+                new Point(9, 3), 
+                new Point(4, 12) 
+            };
+            int count = RectanglesCounter.CountRectangles(points);
+            Console.WriteLine($"Number of rectangles: {count}");
+        }
+    }
+}
---
+++ Rectangles/RectanglesCounter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Rectangles
+{
+    public static class RectanglesCounter
+    {
+        public static int CountRectangles(Point[] points)
+        {
+            var count = 0;
+            if (points.Length < 4)
+            {
+                return count;
+            }
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    Point p1 = points[i];
+                    Point p2 = points[j];
+
+                    if (p1.X == p2.X || p1.Y == p2.Y)
+                        continue;
+
+                    Point p3 = new Point(p1.X, p2.Y);
+                    Point p4 = new Point(p2.X, p1.Y);
+
+                    if (points.Contains(p3) && points.Contains(p4))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count / 2;
+        }
+    }
+}
